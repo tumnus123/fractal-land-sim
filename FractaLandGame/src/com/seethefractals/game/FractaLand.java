25c7f0aaea817f243303cf5c3b8813d8811d244a
tumnus123 <tumnus123@gmail.com>
2014-08-04 06:28:22 -0600

    Fixed alpha; started mset calc

--- FractaLandGame/src/com/seethefractals/game/FractaLand.java
+++ FractaLandGame/src/com/seethefractals/game/FractaLand.java
@@ -1,3 +1,4 @@
+
 package com.seethefractals.game;
 
 import java.util.ArrayList;
@@ -8,31 +9,31 @@ import java.util.*;
 
 /**
   * @author tumnus123
-  * @author tarah.west
+   * @author tarah.west
   * 
   */
 public class FractaLand
 {
-	// this class contains an ArrayList of ArrayLists
-	// and methods for performing ops on that AL
-	// add a row/col, remove a row/col, suppress a row/col,
-	// erodeHeights...
-	//
-	// this class depends on the FracNode class
-	//
-	// the FracNode class represents a point as a
-	// position on a grid of unevenly-spaced points
-	// Properties include iXpos, iYpos, fHeight, fOffset  
-	// Origin is center of grid
-	// Offset is dist away from next innermost node
-	// This allows node spacing to vary by dist to origin
-	//
-	// once a node's height is calculated and eroded, 
-	// it does not change while the node exists.
-	// New rows/cols of nodes are added as needed as a 
-	// result of magnification
-	// Newly added nodes are fully suppressed at initialization,
-	// meaning their initial height is the 
+// this class contains an ArrayList of ArrayLists
+// and methods for performing ops on that AL
+// add a row/col, remove a row/col, suppress a row/col,
+// erodeHeights...
+//
+// this class depends on the FracNode class
+//
+// the FracNode class represents a point as a
+// position on a grid of unevenly-spaced points
+// Properties include iXpos, iYpos, fHeight, fOffset  
+// Origin is center of grid
+// Offset is dist away from next innermost node
+// This allows node spacing to vary by dist to origin
+//
+// once a node's height is calculated and eroded, 
+// it does not change while the node exists.
+// New rows/cols of nodes are added as needed as a 
+// result of magnification
+// Newly added nodes are fully suppressed at initialization,
+// meaning their initial height is the
 
 	private ArrayList<ArrayList<FracNode>> fl;
 
@@ -54,17 +55,17 @@ public class FractaLand
 		paint.setColor(Color.WHITE);
 		paint.setStyle(Paint.Style.FILL);
 
-		// AL must have a center node, so
-		// double radius and add one
+// AL must have a center node, so
+// double radius and add one
 		this.iRadius = iRadius;
 		int i = (iRadius * 2) + 1;
 
-		// Spacing is the screen space between nodes
+// Spacing is the screen space between nodes
 		this.fSpacing = fSpacing;
 		this.fMaxSpacing = fSpacing * 2;
 		this.fMaxViewDist = fMaxViewDist;
 
-		// create and initialize
+// create and initialize
 		fl = new ArrayList<ArrayList<FracNode>>(i);
 		for (int j = 0; j < i; j++)
 		{
@@ -76,8 +77,8 @@ public class FractaLand
 			}
 			fl.add(col);
 		}
-		
-		// init offsets
+
+// init offsets
 		fOffsetX = 0f;
 		fOffsetY = 0f;
 		fOffsetMag = 0f;
@@ -123,15 +124,15 @@ public class FractaLand
 		{
 			fIncrementMag = 0.2f; 
 			fDeltaMag = fDeltaMag - fIncrementMag;
-			fSpacing += fIncrementMag;			
+			fSpacing += fIncrementMag;	
 			return;
-		} 
+		}
 
 		if (fSpacing > fMaxSpacing)
 		{
-			// 1) generate a new fl
+// 1) generate a new fl
 			iRadius *= 2;
-			int iWidth  = (iRadius * 2) + 1;
+			int iWidth = (iRadius * 2) + 1;
 			fSpacing /= 2;
 			ArrayList<ArrayList<FracNode>> newfl;
 			newfl = new ArrayList<ArrayList<FracNode>>(iWidth);
@@ -144,7 +145,7 @@ public class FractaLand
 				}
 				newfl.add(col);
 			}
-			// 2) pop with old fl values and sub
+// 2) pop with old fl values and sub
 			int xF = 0;
 			int yF = 0;
 			FracNode fnOld, fnNew;
@@ -214,7 +215,7 @@ public class FractaLand
 		for (int i=0;i < fl.size();i++)
 		{
 			thisAL = fl.get(i);
-			thisAL.add(index, new FracNode(0d, 0d, Color.YELLOW, 255));
+			thisAL.add(index, new FracNode(0d, 0d, Color.WHITE, 255));
 		}
 	}
 	private void removeRowAt(int index)
@@ -231,7 +232,7 @@ public class FractaLand
 		ArrayList<FracNode> col = new ArrayList<FracNode>(fl.size());
 		for (int y=0;y < fl.size();y++)
 		{
-			col.add(new FracNode(0d, 0d, Color.BLUE, 255));
+			col.add(new FracNode(0d, 0d, Color.WHITE, 255));
 		}
 		fl.add(index, col);
 	}
@@ -252,7 +253,7 @@ public class FractaLand
 		c.drawLine(ctrX - 5, ctrY, ctrX + 5, ctrY, paint);
 		c.drawLine(ctrX, ctrY - 5, ctrX, ctrY + 5, paint);
 
-		// draw the FracNodes centered at 0,0
+// draw the FracNodes centered at 0,0
 		ArrayList row;
 		FracNode node;
 		int iAlpha;
@@ -263,28 +264,39 @@ public class FractaLand
 				row = fl.get(x + iRadius);
 				node = (FracNode) row.get(y + iRadius);
 				paint.setColor(node.getIter());
-				// increment node alphas while zooming
+// increment node alphas while zooming
 				iAlpha = node.getAlpha();
 				if(fDeltaMag>1.0f){
-					if (iAlpha < 254){
-						node.setAlpha(iAlpha + 5);
+					if (iAlpha < 250){
+						node.setAlpha(iAlpha + 10);
+					} else {
+						node.setAlpha(255);
 					}
 				}
 				paint.setAlpha(iAlpha);
 				float x1 = ctrX + (x * fSpacing) + fOffsetX;
 				float y1 = ctrY + (y * fSpacing) + fOffsetY;
 				c.drawCircle(x1, y1, 1.5f, paint);
-				
-				
-				
-				
 			}
-		}		
+		}	
+	}
+
+	private FracNode calcIter(FracNode node) {
+		
+		int iterMax = 300;
+		double zx = 0.0, zy = 0.0;
+		double zx2 = 0.0, zy2 = 0.0;
+		int iter = 0;
+		while (iter < iterMax && zx2 + zy2 < 4.0) {
+				
+		}	
+
+		return node;
 	}
 
 	public FracNode getXY(int x, int y)
 	{
-		// center node is 0,0
+// center node is 0,0
 		int offX = x + iRadius;
 		int offY = y + iRadius;
 		return fl.get(offX).get(offY);
@@ -292,7 +304,7 @@ public class FractaLand
 
 	public void setXY(int x, int y, Double val)
 	{
-		// center node is 0,0
+// center node is 0,0
 		int offX = x + iRadius;
 		int offY = y + iRadius;
 		fl.get(offX).set(offY, new FracNode());
@@ -300,7 +312,7 @@ public class FractaLand
 
 	public void setRadius(int iRadius)
 	{
-		//TODO: make private
+//TODO: make private
 		this.iRadius = iRadius;
 	}
 	public int getRadius()
@@ -310,7 +322,7 @@ public class FractaLand
 
 	public void setSpacing(float fSpacing)
 	{
-		//TODO: make private
+//TODO: make private
 		this.fSpacing = fSpacing;
 	}
 	public float getSpacing()
@@ -318,4 +330,5 @@ public class FractaLand
 		return fSpacing;
 	}
 
-}
+	}
+
